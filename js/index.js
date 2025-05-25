c39267097d3d2610fec494425f1ebbea6be751cb
m7mdbahaa2 <161660318+m7mdbahaa2@users.noreply.github.com>
2025-05-26 00:34:33 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var qoutes = [
+var quotes = [
   "Be yourself; everyone else is already taken.",
   "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
   "So many books, so little time.",
@@ -7,17 +7,18 @@ var qoutes = [
   "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
   "You only live once, but if you do it right, once is enough.",
 ];
-var stored=-100;
+var stored = -100;
 function getQoute() {
   var random = Math.random();
-  var index = Math.floor(random * qoutes.length);
-  while(index==stored){
-    var random = Math.random();
-    var index=Math.floor(random * quotes.length);}
-    
-console.log(stored);
-var selected = qoutes[index];
- stored= index;
+  var index = Math.floor(random * quotes.length);
+  while (index == stored) {
+    random = Math.random();
+    index = Math.floor(random * quotes.length);
+  }
+
+  console.log(stored);
+  var selected = quotes[index];
+  stored = index;
 
   console.log(selected);
   document.getElementById("quote").innerText = selected;
